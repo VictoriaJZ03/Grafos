8a9943dbb40a016c06cabd9ccc22951942e10a9a
Victoria Jimenez <kodakenergy@gmail.com>
2023-10-24 22:51:06 -0500

    new Version added "bfs"&& "dfs"

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import javax.swing.*;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,80 +8,81 @@ public class Main {
         System.out.println("Bienvenida a mi programa para la creacion de un grafo");
         int numVertices;
         int opc;
-        ArrayList<String> cont= new ArrayList<>();
+        ArrayList<String> cont;
         System.out.println("Da el numero de vertices maximo");
         numVertices= kk.nextInt();
         kk.nextLine();
         GrafoMatriz grafo1=new GrafoMatriz(numVertices);
-        Vertice vert;
         do{
-            System.out.println("Digita la opcion que deseas hacer:" +"\n 1.Agregar vertice"+"\n 2.Añadir arco" +"\n 3.Buscar adyacencia" +"\n 4.Comprobar camino "+"\n 5.Recorrido BFS  "  +"\n 6.Recorrido por dfs" +"\n 7.Salir");
+            System.out.println("""
+                    Digita la opcion que deseas hacer:
+                     1.Agregar vertice
+                     2.Añadir arco
+                     3.Buscar adyacencia
+                     4.Comprobar camino\s
+                     5.Recorrido BFS \s
+                     6.Recorrido por dfs
+                     7.Salir""");
             opc= kk.nextInt();
             kk.nextLine();
-            switch (opc){
-                case 1:
+            switch (opc) {
+                case 1 -> {
                     System.out.print("Da el contenido del vertice");
-                    String ver= kk.nextLine();
+                    String ver = kk.nextLine();
                     grafo1.AddVertex(ver);
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Da el nodo origen");
-                    String or= kk.nextLine();
+                    String or = kk.nextLine();
                     System.out.println("Da el nodo destino");
-                    String dest= kk.nextLine();
-                    grafo1.newArco(or,dest);
+                    String dest = kk.nextLine();
+                    grafo1.newArco(or, dest);
                     System.out.println("Arco creado");
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("Da el nodo origen");
-                    String origin= kk.nextLine();
+                    String origin = kk.nextLine();
                     System.out.println("Da el nodo destino");
-                    String destt= kk.nextLine();
-                    grafo1.adyacente(origin,destt);
-                    if(grafo1.adyacente(origin,destt)==true){
+                    String destt = kk.nextLine();
+                    grafo1.adyacente(origin, destt);
+                    if (grafo1.adyacente(origin, destt)) {
                         System.out.println("Hay camino");
-                    }
-                    else{
+                    } else {
                         System.out.println("No hay camino");
                     }
-                    break;
-                case 4:
+                }
+                case 4 -> {
                     System.out.println("da el nodo origen ");
-                    String o= kk.nextLine();
+                    String o = kk.nextLine();
                     System.out.println("Da el nodo destino");
-                    String d= kk.nextLine();
-                    grafo1.existeCamino(o,d);
-                    if (grafo1.existeCamino(o, d)==true) {
+                    String d = kk.nextLine();
+                    grafo1.existeCamino(o, d);
+                    if (grafo1.existeCamino(o, d)) {
                         System.out.println("Hay camino");
-                    }
-                    else {
+                    } else {
                         System.out.println("There´s no way!");
                     }
-                    break;
-                case 5:
+                }
+                case 5 -> {
                     System.out.println("Dame el vertice por el cual quieres partir ");
-                    String vo= kk.nextLine();
-                    cont= grafo1.bfs(vo);
+                    String vo = kk.nextLine();
+                    cont = grafo1.bfs(vo);
                     System.out.println("Este es el recorrido ");
-                    for(int i=0; i<cont.size();i++){
-                        System.out.println(vo +"-->" + cont.get(i));
+                    for (String s : cont) {
+                        System.out.println(vo + "-->" + s);
                     }
-                  break;
-                case 6:
+                }
+                case 6 -> {
                     System.out.println("Da el vertice del cual quieres partir ");
-                    String vor= kk.nextLine();
-                    cont=grafo1.dfs(vor);
+                    String vor = kk.nextLine();
+                    cont = grafo1.dfs(vor);
                     System.out.println("Este es el reccorrido ´por dfs");
-                    for(int i=0; i< cont.size(); i++)
-                    {
-                        System.out.println(vor +"-->" +cont.get(i));
+                    for (String s : cont) {
+                        System.out.println(vor + "-->" + s);
                     }
-                    break;
-                case 7:
-                    System.out.println("Saliedno...");
-                    break;
-                default:
-                    System.out.println("Ingresa una opcion correcta");
+                }
+                case 7 -> System.out.println("Saliedno...");
+                default -> System.out.println("Ingresa una opcion correcta");
             }
 
         }while (opc!=7);
